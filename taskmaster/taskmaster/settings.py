d7a47fc448c45ba687e0b582d181d324519c42d3
Ikaelelo Motlhako <ikaelelo.motlhako@gmail.com>
2025-01-07 21:14:00 +0200

    Imported load_dotenv from dotenv to load environment variables from .env file in settings.py.

--- taskmaster/taskmaster/settings.py
+++ taskmaster/taskmaster/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+from dotenv import load_dotenv
 import os
 from pathlib import Path
 
@@ -19,6 +19,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
+
+# Load environment variables from .env file
+load_dotenv()
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
